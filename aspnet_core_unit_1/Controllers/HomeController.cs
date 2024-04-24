b5835788fcb9f5a3b00d002bacb678133ed7a557
veki2102 <154544653+veki2102@users.noreply.github.com>
2024-04-24 14:31:46 +0200

    Add files via upload

---
+++ aspnet_core_unit_1/Controllers/HomeController.cs
@@ -0,0 +1,51 @@
+﻿using aspnet_core_unit_1.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace aspnet_core_unit_1.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public ActionResult CheckCountValue(int i)
+        {
+            if (i < 20)
+            {
+                //Zamislite da ovdje ide neka poslovna logika
+            }
+            else
+            {
+                throw (new Exception("Broj je izvan raspona"));
+            }
+
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
