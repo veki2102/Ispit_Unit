20006059c59f32b78b7c19381c7a1fc88974a37a
veki2102 <154544653+veki2102@users.noreply.github.com>
2024-04-24 14:41:23 +0200

    Add files via upload

---
+++ HomeControllerTests.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using aspnet_core_unit_1.Controllers;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+
+namespace aspnet_core_unit_1_test
+{
+    public class HomeControllerTests
+    {
+        [Theory]
+        [InlineData(500)]
+        [InlineData(2)]
+        
+
+
+        public void CheckCountValueTest(int i)
+        {
+           
+            var controller = new HomeController(null);
+
+            var result=controller.CheckCountValue(i);  
+
+            Assert.NotNull(result); 
+
+
+        }
+
+    }
+}
